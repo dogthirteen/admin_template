a6b6a2df915a25e1b6b364057d7b8f4b6f768e0d
佘李伟 <sheliwei17@163.com>
2021-05-11 10:14:30 +0800

    fix

--- admin-api/app.js
+++ admin-api/app.js
@@ -1,13 +1,13 @@
 // 创建服务器，监听用户请求
 //引入核心模块
-const express = require('express')
-const fs = require('fs')
-const app = express()
+const express = require('express');
+const fs = require('fs');
+const app = express();
 
 //添加监听
 app.listen(3000, () => {
-    console.log('http://127.0.0.1:3000')
-})
+    console.log('http://127.0.0.1:3000');
+});
 
 app.post('/login', function (req, res) {
     fs.readFile('./user.json', 'utf-8', (error, responseData) => {
@@ -18,9 +18,10 @@ app.post('/login', function (req, res) {
                 ...JSON.parse(responseData)
             },
             msg:"登陆成功"
-        })
-    })
-})
+        });
+    });
+});
+
 app.get('/get_sync_routes', function (req, res) {
     fs.readFile('./syncRoutes.json', 'utf-8', (error, responseData) => {
         if (error) return console.log(error.message);
@@ -30,9 +31,9 @@ app.get('/get_sync_routes', function (req, res) {
                 ...JSON.parse(responseData)
             },
             msg:"请求成功"
-        })
-    })
-})
+        });
+    });
+});
 app.post('/upload', function (req, res) {
     fs.readFile('./syncRoutes.json', 'utf-8', (error, responseData) => {
         if (error) return console.log(error.message);
@@ -42,6 +43,6 @@ app.post('/upload', function (req, res) {
                 ...JSON.parse(responseData)
             },
             msg:"请求成功"
-        })
-    })
-})
+        });
+    });
+});
